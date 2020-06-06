45e6999e9bbf40203456a83209b96b0e40c69e3f
Jorge Castello <jorgio.castello@gmail.com>
2020-06-06 12:48:34 -0700

    Create sample data for testing purposes

---
+++ eight46/sampleData/data.tsx
@@ -0,0 +1,64 @@
+const organizations =
+[
+  {
+    "id": "3685b542-61d5-45da-9580-162dca725966",
+    "name": "American Red Cross",
+    "alias": "",
+    "ngo_id": "53-0196605",
+    "mission": "The American Red Cross prevents and alleviates human suffering in the face of emergencies by mobilizing the power of volunteers and the generosity of donors.",
+    "street1": "2025 E Street, NW",
+    "street2": "",
+    "city": "Washington",
+    "region": "DC",
+    "postal_code": "20006",
+    "country": "US",
+    "lat": "-77.041248",
+    "lon": "38.897131",
+    "website_url": "http://www.redcross.org/",
+    "profile_url": "https://www.pledgeling.com/organizations/42/american-red-cross",
+    "logo_url": "https://res.cloudinary.com/pledgeling/w_266%2Ch_266%2Cc_fit/prod-media/images%2Fnpos%2Flogos%2F2017%2F2%2F6%2FARC_Logo_Bttn_HorizStkd_RGB.png",
+    "disbursement_type": "standard"
+  },
+  {
+    "id": "3685b542-61d5-45da-9580-162dca725967",
+    "name": "Black Lives Matter",
+    "alias": "",
+    "ngo_id": "53-0196605",
+    "mission": "Black Lives Matter (BLM) is an international human rights movement, originating in the African-American community, that campaigns against violence and systemic racism towards black people",
+    "street1": "2025 E Street, NW",
+    "street2": "",
+    "city": "Washington",
+    "region": "DC",
+    "postal_code": "20006",
+    "country": "US",
+    "lat": "-77.041248",
+    "lon": "38.897131",
+    "website_url": "https://blacklivesmatter.com/what-we-believe/",
+    "profile_url": "https://www.pledgeling.com/organizations/42/american-red-cross",
+    "logo_url": "https://blacklivesmatter.com/wp-content/themes/blm/dist/images/logo-black-lives-matter.png",
+    "disbursement_type": "standard"
+  },
+];
+
+const roundUps = [
+  {
+    date: 'May 15, 2019',
+    vendor: 'Starbucks',
+    price: 4.32,
+    roundUp: 0.68
+  },
+  {
+    date: 'May 29, 2019',
+    vendor: 'Swork',
+    price: 5.89,
+    roundUp: 0.68
+  },
+  {
+    date: 'June 2, 2019',
+    vendor: 'SweetGreen',
+    price: 18.20,
+    roundUp: 0.80
+  }
+];
+
+export default { organizations, roundUps };
