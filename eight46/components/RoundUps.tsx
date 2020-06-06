b7aab4bfdfe8cffed0a2ce37204c32090cd57787
Jorge Castello <jorgio.castello@gmail.com>
2020-06-06 14:27:18 -0700

    Add props / data flow to RoundUps component

--- eight46/components/RoundUps.tsx
+++ eight46/components/RoundUps.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import tailwind from 'tailwind-rn';
 
-export default function RoundUps() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}> RoundUp Component </Text>
-      <Text>List of Recent Transactions Add / Remove</Text>
-    </View>
-  );
+interface Transaction {
+  id: number,
+  date: string,
+  vendor: string,
+  price: number,
+  roundUp: number,
 }
 
+type RoundUpsProps = {
+  transactionData: Array<Transaction>,
+};
+
+const RoundUps: React.FC<RoundUpsProps> = ({ transactionData }) => {
+  return (
+    <FlatList
+    style={tailwind('')}
+    data={transactionData}
+      renderItem={({ item }) => (
+        <View style={tailwind('border-solid border-4 border-gray-600 mt-1 mb-1 flex-row justify-between')}>
+              <Text>{item.date}</Text>
+              <Text>{item.vendor}</Text>
+              <Text>{item.price}</Text>
+              <Text>{item.roundUp}</Text>
+          </View>
+      )}
+    />
+  );
+};
+
 const styles = StyleSheet.create({
-  container: {
+  organizationCard: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: '5%',
   },
   text: {
     color: 'red',
   }
 });
+
+export default RoundUps;
