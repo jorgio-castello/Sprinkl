36736a4d78e21d5634d50195b256328d6859a528
Jorge Castello <jorgio.castello@gmail.com>
2020-06-06 01:02:12 -0700

    Create simple server listening on Port 8460

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const app = express();
+
+const PORT = process.env.port || 8460;
+
+app.listen(3000, (err) => {
+  if (err) {
+    throw new Error(err);
+  } else {
+    console.log(`Server is listening on Port ${PORT}...`);
+  }
+});
