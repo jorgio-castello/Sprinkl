bb1b9434d45fa9c140585a9d7f979e3448e23622
Jorge Castello <jorgio.castello@gmail.com>
2020-06-06 20:07:16 -0700

    Do some work on Donate component

--- eight46/components/Donate.tsx
+++ eight46/components/Donate.tsx
@@ -13,15 +13,3 @@ export default function Donate() {
     </View>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   text: {
-//     color: 'red',
-//   }
-// });
