951a0cb8ac92aa776213c00802bd305c8f8612b2
Jorge Castello <jorgio.castello@gmail.com>
2020-06-08 09:24:53 -0700

    Flow Trending Organizations, create nav bar

--- eight46/App.tsx
+++ eight46/App.tsx
@@ -5,6 +5,7 @@ import { StyleSheet, ScrollView, View, Text } from 'react-native';
 import Organization from './components/Organization';
 import Donate from './components/Donate';
 import RoundUps from './components/RoundUps';
+import TrendingCauses from './components/TrendingCauses';
 
 // Data
 import data from './sampleData/data';
@@ -14,30 +15,34 @@ const { organizations } = data;
 type AppState = {
   organizations: Array<Object>,
   roundUps: Array<Object>,
+  trendingCauses: Array<Object>,
 }
 
 export default class App extends React.Component<{}, AppState> {
   constructor(props: Object) {
     super(props);
     this.state = {
+      trendingCauses: [],
       organizations,
       roundUps: []
     };
   }
 
   componentDidMount() {
-    fetch('http://127.0.0.1:8460/refreshUserData')
+    fetch('http://127.0.0.1:8460/getTrendingData')
       .then(res => res.json())
-      .then(userData => {
-        this.setState({ roundUps: userData });
+      .then(trendingCauses => {
+        this.setState({ trendingCauses });
       });
   }
 
   render() {
     return (
-      <View style={tailwind('border-solid border-4 border-gray-600 flex-1')}>
-
-        <View style={tailwind('my-10')}>
+      <View style={tailwind('flex-1')}>
+        <View>
+          <TrendingCauses trendingCauses={this.state.trendingCauses} />
+        </View>
+        {/* <View style={tailwind('my-10')}>
           <Text style={styles.text}> Organization Cards </Text>
           <Organization organizationData={this.state.organizations}/>
         </View>
@@ -50,22 +55,13 @@ export default class App extends React.Component<{}, AppState> {
         <View style={tailwind('my-10')}>
           <Text style={styles.text}> Transaction Cards </Text>
           <RoundUps transactionData={this.state.roundUps}/>
+        </View> */}
+        <View style={tailwind('w-full bg-blue-700 h-20 z-10 absolute bottom-0 left-0 opacity-75 flex-row')}>
+          <View style={tailwind('w-1/3 h-full border border-blue-600')}></View>
+          <View style={tailwind('w-1/3 h-full border border-blue-600')}></View>
+          <View style={tailwind('w-1/3 h-full border border-blue-600')}></View>
         </View>
-
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: '#fff',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  //   marginTop: '20%',
-  // },
-  text: {
-    color: 'red',
-  }
-});
