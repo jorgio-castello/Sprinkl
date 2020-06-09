3f74d066d3044390904977af1304910fb2649ed6
Jorge Castello <jorgio.castello@gmail.com>
2020-06-08 18:37:13 -0700

    Style recent transactions for donations component

--- eight46/components/RoundUps.tsx
+++ eight46/components/RoundUps.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import tailwind from 'tailwind-rn';
 
+import TransactionCard from './TransactionCard';
+
 interface Transaction {
   transaction_id: string,
   date: string,
@@ -16,33 +18,30 @@ type RoundUpsProps = {
 
 const RoundUps: React.FC<RoundUpsProps> = ({ transactionData }) => {
   return (
-    <FlatList
-    style={tailwind('')}
-    data={transactionData}
-      renderItem={({ item }) => (
-        <View style={tailwind('border-solid border-4 border-gray-600 mt-1 mb-1 flex-row justify-between')}>
-              <Text>{item.date}</Text>
-              <Text>{item.name}</Text>
-              <Text>{item.amount}</Text>
-              <Text>{item.roundUp}</Text>
-          </View>
-      )}
-      keyExtractor={item => item.transaction_id}
-    />
+    <>
+      <Text style={tailwind('ml-4 font-light')}>Recent Transactions</Text>
+      { transactionData.map((transaction, index) => {
+        if ( index < 3) {
+          return (
+            <TransactionCard transactionData={transaction} />
+          );
+        }
+      })}
+      {/* <FlatList
+      style={tailwind('')}
+      data={transactionData}
+        renderItem={({ item }) => (
+          <View style={tailwind('border-solid border-4 border-gray-600 mt-1 mb-1 flex-row justify-between')}>
+                <Text>{item.date}</Text>
+                <Text>{item.name}</Text>
+                <Text>{item.amount}</Text>
+                <Text>{item.roundUp}</Text>
+            </View>
+        )}
+        keyExtractor={item => item.transaction_id}
+      /> */}
+    </>
   );
 };
-
-const styles = StyleSheet.create({
-  organizationCard: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: '5%',
-  },
-  text: {
-    color: 'red',
-  }
-});
 
 export default RoundUps;
