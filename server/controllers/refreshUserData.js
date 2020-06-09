c42117b0649d8a584d6591616b33f5600d574a66
Jorge Castello <jorgio.castello@gmail.com>
2020-06-08 18:42:44 -0700

    Refactor refreshUserData to obtain all data needed

--- server/controllers/refreshUserData.js
+++ server/controllers/refreshUserData.js
@@ -1,11 +1,28 @@
 const { fetchTransactionData } = require('./helpers/refreshUserDataHelpers');
+const { getTrendingData, getPreferencesData } = require('./getTrendingData');
 
 const refreshUserData = (req, res) => {
-  fetchTransactionData((err, transactionData) => {
+  fetchTransactionData((err, roundUps) => {
     if (err) {
       throw new Error(err);
     } else {
-      res.json(transactionData);
+      getTrendingData((err, trendingCauses) => {
+        if (err) {
+          throw new Error(err);
+        } else {
+          let donationsSum = roundUps.map(transaction => transaction.roundUp).reduce((a, b) => a + b);
+
+          getPreferencesData((err, userPreferences) => {
+            if (err) {
+              throw new Error(err);
+            } else {
+              const userData = { donationsSum, userPreferences };
+              res.json({ roundUps, trendingCauses, userData });
+            }
+          });
+
+        }
+      });
     }
   });
 };
